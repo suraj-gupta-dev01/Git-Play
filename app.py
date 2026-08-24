314e7206b4147bc9daa2ac57a4f6582efa2ac666
Suraj <agentgupta216@gmail.com>
2026-08-25 00:16:23 +0530

    decorator practice

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 raw_data = list(map(lambda key: input(f"enter your {key}: "), ["name", "age", "contact", "city"]))
 print(raw_data)
+
+
+
+def filter_integers(func):
+    def wrapper(*args, **kwargs):
+        print(args)
+    return wrapper
+
+
+@filter_integers
+def show_data(data):
+    return data
+
+show_data()
